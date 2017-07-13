9f51f66609fddbecaeec623792b289dc35da50ac
fisland <whereisfisland@163.com>
2017-07-13 18:07:41 +0800

    优化下载

--- tryMeizi/meizi.py
+++ tryMeizi/meizi.py
@@ -3,12 +3,10 @@ from bs4 import BeautifulSoup
 import html5lib
 import os
 import time
-# 保存首页
-def save_file(data,pic_name):
-    save_path = 'pic_name'
-    with open(save_file, 'w') as f:
-        f.write(data)
-
+# 爬的预览页面数量
+preview_page_cnt = 2
+parser = 'html.parser'
+cur_path = os.getcwd() + '/'
 
 url = 'http://www.mzitu.com/page/'
 
@@ -16,10 +14,46 @@ header = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
     }
 
-# 爬的预览页面数量
-preview_page_cnt = 2
-parser = 'html.parser'
-cur_path = os.getcwd() + '/'
+# 保存首页
+def save_file(data, pic_name):
+    save_path = pic_name
+    with open(save_path, 'wb') as f:
+        f.write(data)
+
+# 创建目录
+def create_dir(pic_dir):
+    pic_path = cur_path+dir_name
+    if os.path.exists(pic_path):
+        print('Directory Exist!')
+    else:
+        os.mkdir(pic_path)
+    return pic_path
+
+
+def downlaod(url, headers, try_nums=2):
+    '''下载，可以重连，默认次数为2
+    '''
+    print('开始下载', url)
+    try:
+        html = requests.get(url, header)
+    except expression as e:
+        html = None
+        if try_nums > 0:
+            downlaod(url, header, try_nums -1)
+    return html
+
+def image_src_Downlaod(url, headers, try_nums=2):
+    '''下载，可以重连，默认次数为2
+    '''
+    print('开始下载', url)
+    try:
+        html = requests.get(url, header)
+    except expression as e:
+        html = None
+        if try_nums > 0:
+            downlaod(url, header, try_nums -1)
+    return html
+
 
 
 for cur_page in range(1, int(preview_page_cnt)+1):
@@ -38,31 +72,33 @@ for cur_page in range(1, int(preview_page_cnt)+1):
         # 获取图片数量
         pri_cnt = soup.find('div', class_='pagenavi').find_all('a')[4].get_text()
         # 创建目录
-        pic_path = cur_path+dir_name
-        if os.path.exists(pic_path):
-            print('Directory Exist!')
-        else:
-            os.mkdir(pic_path)
+        pic_path = create_dir(dir_name)
         # 进入目录，开始下载
         os.chdir(pic_path)
         print('下载'+dir_name+'...')
-
         # 遍历获取每页图片的地址
         for pic_index in range(1, int(pri_cnt)+1):
             pic_link = link+'/'+str(pic_index)
-            cur_page = requests.get(pic_link, headers = header)
-            print(pic_link)
+            # cur_page = requests.get(pic_link, headers = header)
+            cur_page = downlaod(pic_link, header)
             soup = BeautifulSoup(cur_page.text, parser)
-            print(soup)
-            pic_src = soup.find('div', 'main-image').find('img')['src']
+            try:
+                pic_src = soup.find('div', 'main-image').find('img')['src']
+            except AttributeError as e:
+                print(e)
+
             pic_name = pic_src.split('/')[-1]
-            with open(pic_name, 'wb') as f:
-                f.write(requests.get(pic_src, headers = header).content)
+
+            save_file(requests.get(pic_src, headers = header).content, pic_name)
+
             print(pic_src)
-            time.sleep(1)
+            time.sleep(0.1)
 
         os.chdir(cur_path)
+    print('爬虫完成')
 
 # main_page = requests.get(url, header)
 # print(main_page.encoding)
 # save_file(main_page.text)
+
+
