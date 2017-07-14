fa27e735a68b2ef766c8ecde12da0df26673645d
fisland <whereisfisland@163.com>
2017-07-14 11:20:48 +0800

    优化目录名字

--- tryMeizi/meizi.py
+++ tryMeizi/meizi.py
@@ -14,15 +14,15 @@ header = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
     }
 
-# 保存首页
+# 保存图片
 def save_file(data, pic_name):
     save_path = pic_name
     with open(save_path, 'wb') as f:
         f.write(data)
 
 # 创建目录
-def create_dir(pic_dir):
-    pic_path = cur_path+dir_name
+def create_dir(pic_dir, pic_detail_name):
+    pic_path = cur_path+pic_detail_name #dir_name+
     if os.path.exists(pic_path):
         print('Directory Exist!')
     else:
@@ -62,9 +62,12 @@ for cur_page in range(1, int(preview_page_cnt)+1):
     # 解析网页
     soup = BeautifulSoup(cur_page.text, parser)
     preview_link_list = soup.find(id='pins').find_all('a', target='_blank')[1::2]
+
     for link in preview_link_list:
+        dir_detail_name = link.get_text()
+        print('link---------->', dir_detail_name)
+
         link = link['href']
-        print(type(link))
 
         dir_name = link.strip('http://www.mzitu.com/').replace('?', '')
         soup = BeautifulSoup(requests.get(link).text, parser)
@@ -72,7 +75,7 @@ for cur_page in range(1, int(preview_page_cnt)+1):
         # 获取图片数量
         pri_cnt = soup.find('div', class_='pagenavi').find_all('a')[4].get_text()
         # 创建目录
-        pic_path = create_dir(dir_name)
+        pic_path = create_dir(dir_name, dir_detail_name)
         # 进入目录，开始下载
         os.chdir(pic_path)
         print('下载'+dir_name+'...')
@@ -92,7 +95,7 @@ for cur_page in range(1, int(preview_page_cnt)+1):
             save_file(requests.get(pic_src, headers = header).content, pic_name)
 
             print(pic_src)
-            time.sleep(0.1)
+            time.sleep(1)
 
         os.chdir(cur_path)
     print('爬虫完成')
